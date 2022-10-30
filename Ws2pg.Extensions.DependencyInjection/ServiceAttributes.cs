e13fb477e1cddfc14b7334d3a1d93119ce5625f6
hexog <egor@egornikulin.me>
2022-10-30 13:20:14 +0400

    Service injection with attributes

---
+++ Ws2pg.Extensions.DependencyInjection/ServiceAttributes.cs
@@ -0,0 +1,16 @@
+﻿namespace Ws2pg.Extensions.DependencyInjection;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class ScopedServiceAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SingletonServiceAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class IgnoreServiceAttribute : Attribute
+{
+}
