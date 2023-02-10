65d6cb03944b0807ccf36b03b72df041e8089713
Egor Nikulin <egor@egornikulin.me>
2023-02-10 21:17:30 +0400

    Simplify service attributes

---
+++ Ws2.DependencyInjection/LifetimeAttributeServiceCollectionExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ws2.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class LifetimeAttributeServiceCollectionExtensions
+{
+	public static IServiceCollection AddServicesByAttributes(this IServiceCollection serviceCollection,
+		params Assembly[] assembliesToAdd)
+	{
+		var types = assembliesToAdd
+			.SelectMany(x => x.DefinedTypes);
+
+		foreach (var type in types)
+		{
+			var serviceAttributes =
+				type.GetCustomAttributes(typeof(ServiceAttribute)).Cast<ServiceAttribute>().ToArray();
+
+			if (serviceAttributes.Length == 0)
+			{
+				continue;
+			}
+
+			var firstServiceAttribute = serviceAttributes[0];
+			var lifetime = firstServiceAttribute.Lifetime;
+			Debug.Assert(Enum.IsDefined(lifetime));
+
+			if (serviceAttributes.Any(x => x.Lifetime != lifetime))
+			{
+				continue;
+			}
+
+			if (lifetime == ServiceLifetime.Singleton)
+			{
+				AddSingletonService(serviceCollection, type, serviceAttributes);
+			}
+			else
+			{
+				foreach (var serviceAttribute in serviceAttributes)
+				{
+					var service = serviceAttribute.Service ?? type;
+					serviceCollection.TryAdd(new ServiceDescriptor(service, type, lifetime));
+				}
+			}
+		}
+
+		return serviceCollection;
+	}
+
+	private static void AddSingletonService(IServiceCollection serviceCollection, TypeInfo type,
+		ServiceAttribute[] serviceAttributes)
+	{
+		serviceCollection.TryAdd(new ServiceDescriptor(type, type, ServiceLifetime.Singleton));
+		foreach (var serviceAttribute in serviceAttributes)
+		{
+			if (serviceAttribute.Service is not null)
+			{
+				serviceCollection.TryAdd(new ServiceDescriptor(serviceAttribute.Service, type));
+			}
+		}
+	}
+}
---
+++ Ws2.DependencyInjection/ServiceAttribute.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ws2.DependencyInjection;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = true)]
+public abstract class ServiceAttribute : Attribute
+{
+	protected ServiceAttribute(Type? service = null)
+	{
+		Service = service;
+	}
+
+	public Type? Service { get; set; }
+	public abstract ServiceLifetime Lifetime { get; }
+}
+
+public class ScopedServiceAttribute : ServiceAttribute
+{
+	public ScopedServiceAttribute(Type? service = null) : base(service)
+	{
+	}
+
+	public override ServiceLifetime Lifetime => ServiceLifetime.Scoped;
+}
+
+public class ScopedServiceAttribute<TService> : ScopedServiceAttribute
+{
+	public ScopedServiceAttribute() : base(typeof(TService))
+	{
+	}
+}
+
+public class SingletonServiceAttribute : ServiceAttribute
+{
+	public SingletonServiceAttribute(Type? service = null) : base(service)
+	{
+	}
+
+	public override ServiceLifetime Lifetime => ServiceLifetime.Singleton;
+}
+
+public class SingletonServiceAttribute<TService> : SingletonServiceAttribute
+{
+	public SingletonServiceAttribute() : base(typeof(TService))
+	{
+	}
+}
+
+public class TransientServiceAttribute : ServiceAttribute
+{
+	public TransientServiceAttribute(Type? service = null) : base(service)
+	{
+	}
+
+	public override ServiceLifetime Lifetime => ServiceLifetime.Transient;
+}
+
+public class TransientServiceAttribute<TService> : TransientServiceAttribute
+{
+	public TransientServiceAttribute() : base(typeof(TService))
+	{
+	}
+}
+
