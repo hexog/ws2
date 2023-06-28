97c7b3f0b9c8b8d90168f85c68a6e4babe0676cb
Egor Nikulin <nikulin.e@kontur.ru>
2023-06-28 20:07:59 +0400

    format

--- Ws2.Data/DataServiceCollectionExtensions.cs
+++ Ws2.Data/DataServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Ws2.Data;
 
--- Ws2.DependencyInjection/ServiceAttributeServiceCollectionExtensions.cs
+++ Ws2.DependencyInjection/ServiceAttributeServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Ws2.DependencyInjection;
