945d622f80bf116aedc024f35c0a07dba0a38b90
Egor Nikulin <egor@egornikulin.me>
2023-11-23 20:58:22 +0400

    Simplify adding singletones in the service collection

--- Ws2.DependencyInjection/ServiceAttributeServiceCollectionExtensions.cs
+++ Ws2.DependencyInjection/ServiceAttributeServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Ws2.DependencyInjection;
@@ -8,59 +9,58 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceAttributeServiceCollectionExtensions
 {
-	public static IServiceCollection AddServicesByAttributes(this IServiceCollection serviceCollection,
-		params Assembly[] assembliesToAdd)
-	{
-		var types = assembliesToAdd
-			.SelectMany(x => x.DefinedTypes);
+    public static IServiceCollection AddServicesByAttributes(this IServiceCollection serviceCollection,
+        params Assembly[] assembliesToAdd)
+    {
+        var types = assembliesToAdd
+            .SelectMany(x => x.DefinedTypes);
 
-		foreach (var type in types)
-		{
-			var serviceAttributes =
-				type.GetCustomAttributes(typeof(ServiceAttribute)).Cast<ServiceAttribute>().ToArray();
+        foreach (var type in types)
+        {
+            var serviceAttributes =
+                type.GetCustomAttributes(typeof(ServiceAttribute)).Cast<ServiceAttribute>().ToArray();
 
-			if (serviceAttributes.Length == 0)
-			{
-				continue;
-			}
+            if (serviceAttributes.Length == 0)
+            {
+                continue;
+            }
 
-			var firstServiceAttribute = serviceAttributes[0];
-			var lifetime = firstServiceAttribute.Lifetime;
-			Debug.Assert(Enum.IsDefined(lifetime));
+            var firstServiceAttribute = serviceAttributes[0];
+            var lifetime = firstServiceAttribute.Lifetime;
+            Debug.Assert(Enum.IsDefined(lifetime));
 
-			if (serviceAttributes.Any(x => x.Lifetime != lifetime))
-			{
-				continue;
-			}
+            if (serviceAttributes.Any(x => x.Lifetime != lifetime))
+            {
+                continue;
+            }
 
-			if (lifetime == ServiceLifetime.Singleton)
-			{
-				AddSingletonService(serviceCollection, type, serviceAttributes);
-			}
-			else
-			{
-				foreach (var serviceAttribute in serviceAttributes)
-				{
-					var service = serviceAttribute.Service ?? type;
-					serviceCollection.TryAdd(new ServiceDescriptor(service, type, lifetime));
-				}
-			}
-		}
+            if (lifetime == ServiceLifetime.Singleton)
+            {
+                AddSingletonService(serviceCollection, type, serviceAttributes);
+            }
+            else
+            {
+                foreach (var serviceAttribute in serviceAttributes)
+                {
+                    var service = serviceAttribute.Service ?? type;
+                    serviceCollection.TryAdd(new ServiceDescriptor(service, type, lifetime));
+                }
+            }
+        }
 
-		return serviceCollection;
-	}
+        return serviceCollection;
+    }
 
-	private static void AddSingletonService(IServiceCollection serviceCollection, TypeInfo type,
-		ServiceAttribute[] serviceAttributes)
-	{
-		serviceCollection.TryAdd(new ServiceDescriptor(type, type, ServiceLifetime.Singleton));
-		foreach (var serviceAttribute in serviceAttributes)
-		{
-			if (serviceAttribute.Service is not null)
-			{
-				serviceCollection.TryAddEnumerable(new ServiceDescriptor(serviceAttribute.Service,
-					p => p.GetRequiredService(type), ServiceLifetime.Singleton));
-			}
-		}
-	}
+    private static void AddSingletonService(IServiceCollection serviceCollection, TypeInfo type,
+        ServiceAttribute[] serviceAttributes)
+    {
+        serviceCollection.TryAdd(new ServiceDescriptor(type, type, ServiceLifetime.Singleton));
+        foreach (var serviceAttribute in serviceAttributes)
+        {
+            if (serviceAttribute.Service is not null)
+            {
+                serviceCollection.TryAddEnumerable(ServiceDescriptor.Singleton(serviceAttribute.Service, type));
+            }
+        }
+    }
 }
