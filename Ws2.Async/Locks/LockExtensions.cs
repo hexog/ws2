2ea37d17287c867125c4d6dfe630c0a2833eb17f
Egor Nikulin <egor@egornikulin.me>
2024-01-12 23:18:17 +0400

    Add lock extensions for common key types

---
+++ Ws2.Async/Locks/LockExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Ws2.EqualityComparison;
+
+namespace Ws2.Async.Locks;
+
+public static class LockExtensions
+{
+	public static ValueTask<ILockHolder> AcquireAsync(
+		this ILock @lock,
+		byte[] bytes,
+		CancellationToken cancellationToken
+	)
+	{
+		return @lock.AcquireAsync(bytes, EqualityComparers.ByteArrayEqualityComparer, cancellationToken);
+	}
+
+	public static ValueTask<ILockHolder> AcquireAsync(
+		this ILock @lock,
+		int key,
+		CancellationToken cancellationToken
+	)
+	{
+		return @lock.AcquireAsync(key, EqualityComparers.Int32EqualityComparer, cancellationToken);
+	}
+
+	public static ValueTask<ILockHolder> AcquireAsync(
+		this ILock @lock,
+		string key,
+		CancellationToken cancellationToken
+	)
+	{
+		return @lock.AcquireAsync(key, StringComparer.Ordinal, cancellationToken);
+	}
+}
