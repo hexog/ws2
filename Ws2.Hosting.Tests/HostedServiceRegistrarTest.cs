0277925d080b1bf747f73cf6ea4270e40f0afcf1
Egor Nikulin <egor@egornikulin.me>
2024-01-28 15:44:41 +0400

    Add hosted service registrar

---
+++ Ws2.Hosting.Tests/HostedServiceRegistrarTest.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ws2.DependencyInjection.Abstractions;
+using Ws2.Hosting.Registrars;
+
+namespace Ws2.Hosting.Tests;
+
+public class HostedServiceRegistrarTest
+{
+    public class HostedService1 : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class HostedService2 : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+
+    public class OtherClass
+    {
+    }
+
+    [Test]
+    public void TestRegisterHostedServices()
+    {
+        var services = new ServiceCollection();
+
+        services.AddServicesByAttributesFromTypes(
+            new[] { typeof(HostedService1), typeof(HostedService2), typeof(OtherClass) },
+            Array.Empty<IServiceAttributeRegistrar>(),
+            new IServiceTypeImplementationRegistrar[] { new HostedServiceRegistrar() }
+        );
+
+        var serviceDescriptors = services.ToList();
+        serviceDescriptors.Count.Should().Be(2);
+        serviceDescriptors[0].ImplementationType.Should().Be(typeof(HostedService1));
+        serviceDescriptors[1].ImplementationType.Should().Be(typeof(HostedService2));
+    }
+}
---
+++ Ws2.Hosting/Registrars/HostedServiceRegistrar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Ws2.DependencyInjection.Abstractions;
+
+namespace Ws2.Hosting.Registrars;
+
+public class HostedServiceRegistrar : IServiceTypeImplementationRegistrar<IHostedService>
+{
+    public void Register(IServiceAttributeRegistrarContext context, Type type)
+    {
+        context.ServiceCollection.TryAddEnumerable(ServiceDescriptor.Singleton(typeof(IHostedService), type));
+    }
+}
