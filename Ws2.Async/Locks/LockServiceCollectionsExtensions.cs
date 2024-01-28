ed7c64c80b808584325e6167af6101002f3dd469
Egor Nikulin <egor@egornikulin.me>
2024-01-28 18:24:43 +0400

    Fix method name

--- Ws2.Async/Locks/LockServiceCollectionsExtensions.cs
+++ Ws2.Async/Locks/LockServiceCollectionsExtensions.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class LockServiceCollectionsExtensions
 {
-    public static IServiceCollection AddLocK(this IServiceCollection services, object? lockKey)
+    public static IServiceCollection AddLock(this IServiceCollection services, object? lockKey)
     {
         services.AddKeyedSingleton<ILock>(
             lockKey,
