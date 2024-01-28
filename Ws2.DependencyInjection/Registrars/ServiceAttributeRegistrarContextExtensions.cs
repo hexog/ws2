8f57c659d0da24855481868654822ceead3f6f03
Egor Nikulin <egor@egornikulin.me>
2024-01-28 16:43:00 +0400

    Improve variables names

--- Ws2.DependencyInjection/Registrars/ServiceAttributeRegistrarContextExtensions.cs
+++ Ws2.DependencyInjection/Registrars/ServiceAttributeRegistrarContextExtensions.cs
@@ -10,27 +10,27 @@ internal static class ServiceAttributeRegistrarContextExtensions
 {
     public static void RegisterByServiceAttribute(
         this IServiceRegistrarContext context,
-        Type implementation,
+        Type implementationType,
         ServiceAttribute serviceAttribute
     )
     {
-        if (!context.IsValidImplementationType(implementation))
+        if (!context.IsValidImplementationType(implementationType))
         {
             return;
         }
 
-        var serviceLifetime = serviceAttribute.Lifetime;
-        Debug.Assert(Enum.IsDefined(serviceLifetime));
+        var lifetime = serviceAttribute.Lifetime;
+        Debug.Assert(Enum.IsDefined(lifetime));
         var serviceKey = serviceAttribute.ServiceKey;
-        var service = serviceAttribute.Service
+        var serviceType = serviceAttribute.Service
             ?? context.FindType(serviceAttribute.ServiceTypeName);
-        if (service is null)
+        if (serviceType is null)
         {
-            context.ServiceCollection.TryAdd(new ServiceDescriptor(implementation, serviceKey, implementation, serviceLifetime));
+            context.ServiceCollection.TryAdd(new ServiceDescriptor(implementationType, serviceKey, implementationType, lifetime));
         }
         else
         {
-            context.ServiceCollection.TryAddEnumerable(new ServiceDescriptor(service, serviceKey, implementation, serviceLifetime));
+            context.ServiceCollection.TryAddEnumerable(new ServiceDescriptor(serviceType, serviceKey, implementationType, lifetime));
         }
     }
 }
