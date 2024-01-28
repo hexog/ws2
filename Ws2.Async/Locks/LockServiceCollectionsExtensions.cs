738ac3b3667d36cf25f8cb7277a043f3bf2ce1c4
Egor Nikulin <egor@egornikulin.me>
2024-01-28 18:10:57 +0400

    Add lock services extension

---
+++ Ws2.Async/Locks/LockServiceCollectionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Ws2.Async.Locks;
+using Ws2.Async.Locks.PooledLocks;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class LockServiceCollectionsExtensions
+{
+    public static IServiceCollection AddLocK(this IServiceCollection services, object? lockKey)
+    {
+        services.AddKeyedSingleton<ILock>(
+            lockKey,
+            static (_, _) => new PooledLock(new SemaphoreSlimPool())
+        );
+
+        return services;
+    }
+}
